f54ec976a2b2ff5a7fbfe8bc8388a128ead2d3b2
Rajesh Kumar <rajeshtezu90@gmail.com>
2020-01-31 23:22:24 +0530

    Added a contact app.

---
+++ Contacts/Contacts/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Contacts.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Contacts {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		List<Contact> contacts;
+
+		public MainWindow() {
+			InitializeComponent();
+			contacts = new List<Contact>();
+
+			ReadDatabase();
+		}
+
+		private void newContact_Click(object sender, RoutedEventArgs e) {
+			NewContactWindow newContactWindow = new NewContactWindow();
+			newContactWindow.ShowDialog();
+
+			ReadDatabase();
+		}
+
+		public void ReadDatabase() {
+			using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath)) {
+				conn.CreateTable<Contact>();
+				contacts = conn.Table<Contact>().ToList().OrderBy(contact => contact.Name).ToList();
+			}
+
+			if (contacts != null) {
+				//foreach(var contact in contacts) {
+				//	contactsListView.Items.Add(new ListViewItem() { 
+				//		Content = contact
+				//	});
+				//}
+
+				contactsListView.ItemsSource = contacts;
+			}
+		}
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e) {
+			TextBox searchTextBox = sender as TextBox;
+			var filteredList = contacts.Where(contact => contact.Name.ToLower().Contains(searchTextBox.Text.ToLower())).ToList();
+
+			//var filteredList2 = (from contact2 in contacts
+			//										 where contact2.Name.ToLower().Contains(searchTextBox.Text.ToLower())
+			//										 orderby contact2.Email
+			//										 select contact2
+ 		//											).ToList(); // Using LINQ (Language integrated query)
+
+			contactsListView.ItemsSource = filteredList;
+		}
+
+		private void contactsListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			Contact selectedContact = (Contact)contactsListView.SelectedItem;
+
+			if (selectedContact != null) {
+				ContactDetailsWindow contactDetailsWindow = new ContactDetailsWindow(selectedContact);
+				contactDetailsWindow.ShowDialog();
+			}
+
+			ReadDatabase();
+		}
+	}
+}
---
+++ Contacts/Contacts/Screens/ContactDetailsWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Contacts.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Contacts {
+	/// <summary>
+	/// Interaction logic for ContactDetailsWindos.xaml
+	/// </summary>
+	public partial class ContactDetailsWindow : Window {
+		Contact contact;
+		public ContactDetailsWindow(Contact contact) {
+			InitializeComponent();
+
+			this.contact = contact;
+			nameTextInput.Text = contact.Name;
+			emailTextInput.Text = contact.Email;
+			phoneTextInput.Text = contact.Phone;
+		}
+
+		private void deleteButton_Click(object sender, RoutedEventArgs e) {
+			using (SQLiteConnection connection = new SQLiteConnection(App.databasePath)) {
+				connection.CreateTable<Contact>();
+				connection.Delete(contact);
+			}
+			
+			Close();
+		}
+
+		private void updateButton_Click(object sender, RoutedEventArgs e) {
+			contact.Name = nameTextInput.Text;
+			contact.Email = emailTextInput.Text;
+			contact.Phone = phoneTextInput.Text;
+
+			using (SQLiteConnection connection = new SQLiteConnection(App.databasePath)) {
+				connection.CreateTable<Contact>();
+				connection.Update(contact);
+			}
+
+			Close();
+		}
+	}
+}
