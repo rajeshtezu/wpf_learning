e29077e129219ffb8d03a9b7a5f57be5910b679a
Rajesh Kumar <rajeshtezu90@gmail.com>
2020-02-01 01:15:18 +0530

    Added reusable component. Improved styles.

---
+++ Contacts/Contacts/Controls/ContactControl.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Contacts.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Contacts.Controls {
+	/// <summary>
+	/// Interaction logic for ContactControl.xaml
+	/// </summary>
+	public partial class ContactControl : UserControl {
+		public ContactControl() {
+			InitializeComponent();
+		}
+
+		//private Contact contact;
+
+		//public Contact Contact {
+		//	get { return contact; }
+		//	set { 
+		//		contact = value;
+		//		nameTextBlock.Text = contact.Name;
+		//		emailTextBlock.Text = contact.Email;
+		//		phoneTextBlock.Text = contact.Phone;
+		//	}
+		//}
+
+		public Contact Contact {
+			get { return (Contact)GetValue(ContactProperty); }
+			set { SetValue(ContactProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for Contact.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty ContactProperty =
+				DependencyProperty.Register(
+					"Contact",
+					typeof(Contact),
+					typeof(ContactControl),
+					new PropertyMetadata(
+						new Contact(){ Name = "FName", Email = "email", Phone = "9876543210" }, 
+						SetText
+					)
+				);
+
+		private static void SetText(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+			ContactControl control = d as ContactControl;
+
+			if (control != null) {
+				control.nameTextBlock.Text = (e.NewValue as Contact).Name;
+				control.emailTextBlock.Text = (e.NewValue as Contact).Email;
+				control.phoneTextBlock.Text = (e.NewValue as Contact).Phone;
+			}
+		}
+	}
+}
